affdf210704da8fcb73fe37e87b037dfa0bf3ef8
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2022-05-19 00:20:18 +0300

    v0.1.0: impl time dependent random gauss distibuted wallpaper

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
-use std::time;
-use std::thread;
-use std::{path::{Path, PathBuf}, process::{Command, Output}, io::Error, env::{args, self}};
+/// damn moody wallpaper autochanger
 
-use rand::seq::SliceRandom;
-use walkdir::WalkDir;
+use std::env::{args, self};
+use std::f32::consts::TAU;
+use std::io::Error;
+use std::path::{Path, PathBuf};
+use std::process::{Command, Output};
+use std::thread;
+use std::time;
+
+use chrono::{NaiveTime, Timelike};
 use image::GenericImageView;
+use rand::{Rng, thread_rng};
+use walkdir::WalkDir;
+
 
 
 enum DEWM {
-    Gnome, Sway
+    Budgie,
+    Cinnamon,
+    Deepin,
+    Gnome,
+    Kde,
+    Lxde,
+    Lxqt,
+    Mate,
+    Xfce,
+
+    Awesome,
+    Bspwm,
+    Dwm,
+    I3,
+    Qtile,
+    Sway,
+    Wayfire,
+    Xmonad,
 }
+
 
 
 pub trait ExtensionVecU8ToString {
@@ -20,6 +46,69 @@ impl ExtensionVecU8ToString for Vec<u8> {
         String::from_utf8(self).unwrap()
     }
 }
+
+
+
+fn random_gauss(mu: f32, sigma: f32) -> f32 {
+    // from python's `random.gauss`:
+    // When x and y are two variables from [0, 1), uniformly distributed, then
+    // cos(2*pi*x)*sqrt(-2*ln(1-y))
+    // sin(2*pi*x)*sqrt(-2*ln(1-y))
+    // are two *independent* variables with normal distribution (mu=0, sigma=1).
+    let mut rng = thread_rng();
+    let x: f32 = rng.gen_range(0.0..1.0);
+    let y: f32 = rng.gen_range(0.0..1.0);
+    let t: f32 = (TAU*x).cos() * (-2.0*(1.0-y).ln()).sqrt();
+    mu + t * sigma
+}
+
+
+
+fn time_to_desired_brightness(time: NaiveTime) -> f32 {
+    let hour = time.hour();
+    match hour {
+        _ if (5 <= hour && hour < 21) => { // day
+            0.7
+        }
+        _ if (20 <= hour && hour < 21) => { // evening
+            0.2
+        }
+        _ if (21 <= hour && hour < 22) => { // night
+            0.1
+        }
+        _ if (22 <= hour && hour < 24) || (0 <= hour && hour < 6) => { // deep night
+            0.05
+        }
+        _ => { unreachable!() }
+    }
+}
+
+
+
+fn smart_choose(wallpapers: &Vec<Wallpaper>) -> Wallpaper {
+    assert!(wallpapers.len() > 0);
+    let desired_brightness: f32 = time_to_desired_brightness(chrono::Local::now().time());
+    println!("desired_brightness: {desired_brightness}");
+
+    let mut random_brightness: Option<f32> = None;
+    while random_brightness.is_none() {
+        let rb: f32 = random_gauss(desired_brightness, desired_brightness/2.0);
+        if 0.0 <= rb && rb <= 1.0 {
+            random_brightness = Some(rb);
+        }
+    }
+    let random_brightness: f32 = random_brightness.unwrap();
+    println!("random_brightness: {random_brightness}");
+
+    let mut closest_i: usize = 0;
+    for i in 0..wallpapers.len() {
+        if (wallpapers[i].brightness-random_brightness).abs() < (wallpapers[closest_i].brightness-random_brightness).abs() {
+            closest_i = i;
+        }
+    }
+    wallpapers[closest_i].clone()
+}
+
 
 
 fn get_de_wm() -> DEWM {
@@ -33,11 +122,14 @@ fn get_de_wm() -> DEWM {
             DEWM::Gnome
         }
         _ if xdg_current_desktop.ends_with("SWAY") => {
+            todo!("Check is this is correct way to check if this is sway");
             DEWM::Sway
         }
         _ => { todo!() }
     }
 }
+
+
 
 fn set_wallpaper(path: &Path) -> Result<Output, Error> {
     assert!(path.to_str().unwrap().len() > 0);
@@ -63,6 +155,7 @@ fn set_wallpaper(path: &Path) -> Result<Output, Error> {
         DEWM::Sway => {
             todo!()
         }
+        _ => { todo!() }
     }
 }
 
@@ -72,15 +165,15 @@ fn calc_image_brightness(path: &PathBuf) -> Option<f32> {
     let image = image.unwrap();
     let mut brightness: u64 = 0;
     for (_w, _h, pixel) in image.pixels() {
-        // brightness += pixel.0[0] as u64; // alpha
-        brightness += pixel.0[1] as u64;
-        brightness += pixel.0[2] as u64;
-        brightness += pixel.0[3] as u64;
+        brightness += pixel.0[0] as u64; // red
+        brightness += pixel.0[1] as u64; // green
+        brightness += pixel.0[2] as u64; // blue
+        // brightness += pixel.0[3] as u64; // alpha
     }
     Some((brightness as f64 / (4.0 * 255.0 * image.dimensions().0 as f64 * image.dimensions().1 as f64)) as f32)
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Wallpaper {
     path: PathBuf,
     brightness: f32,
@@ -104,8 +197,7 @@ impl Config {
     fn init_wallpapers(&mut self) {
         for entry in WalkDir::new(self.wallpapers_path.clone().unwrap()) {
             if entry.as_ref().unwrap().path().is_dir() { continue; }
-            println!("{}", entry.as_ref().unwrap().path().display());
-            let path: PathBuf = entry.unwrap().path().to_path_buf();
+            let path: PathBuf = entry.as_ref().unwrap().path().to_path_buf();
             let brightness: Option<f32> = calc_image_brightness(&path);
             if brightness.is_none() {
                 let path_str: String = path.display().to_string();
@@ -113,6 +205,7 @@ impl Config {
                 continue;
             }
             let brightness: f32 = brightness.unwrap();
+            println!("{}", path.display().to_string());
             println!("brightness = {brightness}");
             self.wallpapers.push(Wallpaper { path, brightness });
         }
@@ -165,17 +258,18 @@ fn main() {
         }
     }
 
-    println!("config = {config:#?}");
+    println!("config = {config:#?}\n");
 
+    println!("Initing wallpapers...");
     config.init_wallpapers();
 
     loop {
-        // TODO: impl time dependent wallpaper chooser
-        let random_wallpaper: &Wallpaper = &config.wallpapers.choose(&mut rand::thread_rng()).unwrap();
+        println!();
+        let random_wallpaper: &Wallpaper = &smart_choose(&config.wallpapers);
         let path_str: String = random_wallpaper.path.display().to_string();
-        println!("setting wallpaper: {path_str}");
+        println!("Setting wallpaper: {path_str}");
         set_wallpaper(&random_wallpaper.path).unwrap();
-        println!("Slepping...");
+        println!("Slepping {d}s...", d=config.delay.unwrap());
         thread::sleep(time::Duration::from_secs(config.delay.unwrap() as u64));
     }
 }
